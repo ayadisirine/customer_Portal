c480ab3828ec62f8719b9acdea23321b4377f454
ayadisirine <sirine.ayadi93@gmail.com>
2021-04-07 20:30:04 -0500

    first commit

---
+++ Customers_Portal/Controllers/AccountController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Customers_Portal.Models;
+using System.Net.Http;
+using System.Net;
+
+namespace Customers_Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                              SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+           // ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(requestUri:$"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Customers/{model.Email}");
+            
+
+            if (ModelState.IsValid && response.StatusCode ==HttpStatusCode.OK && response.Content.Headers.ContentLength>2)
+            {
+                var user = new IdentityUser 
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(model);
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+
+            //var branch = new Branch
+            //{
+            //    branchName = "Regie",
+            //    address = "Naval"
+
+            //};
+            //branchContext.Branch.Add(branch);
+            //branchContext.SaveChanges();
+
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, user.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(user);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
