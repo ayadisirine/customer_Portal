3d3d8be4e72913e228968eaa2f1e727ce6a5361f
ayadisirine <sirine.ayadi93@gmail.com>
2021-04-09 22:22:40 -0500

    final version

--- Customers_Portal/Controllers/HomeController.cs
+++ Customers_Portal/Controllers/HomeController.cs
@@ -26,17 +26,19 @@ namespace Customers_Portal.Controllers
 
         public async Task<IActionResult> Index(string id)
         {
-            /*    var httpClient = new HttpClient();
+            if (id == null)
+                return View();
+
+                var httpClient = new HttpClient();
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var responseGet = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Customers/id/{id}");
-                //string response = await responseGet.Content.ReadAsStringAsync();
-
                 List<UesrModel> res = await responseGet.Content.ReadAsAsync<List<UesrModel>>();
 
-                ViewData["username"] = res[0].companyName ;*/
+                ViewData["username"] = res[0].technicalAuthorityEmail ;
 
             this.iduser = id;
             ViewData["id"] = id;
+
             return View();
         }
 
@@ -90,7 +92,7 @@ namespace Customers_Portal.Controllers
                         List<ElevatorModel> resElevator = await responseGetElevator.Content.ReadAsAsync<List<ElevatorModel>>();
                         ViewBag.ElevatorTable += $" <br/><br/> Related Elevators of Columns : {resColumns[k].Id} : ";
                         ViewBag.ElevatorTable += $"<table border=1 width=100% > ";
-                        ViewBag.ElevatorTable += $"<tr><td>id</td><td>Serial Number </td><td>Status</td><td>Id</td> ";
+                        ViewBag.ElevatorTable += $"<tr><td>id</td><td>Serial Number </td><td>Status</td> ";
                         for (int H = 0; H < resElevator.Count; H++)
                         {
                            // ViewBag.ElevatorTable += $"<br> <br> <div style=' margin - left:85px; '> {resElevator[H].Id} </div> <div style='display: inline - block; margin - left:85px; '> {resElevator[H].SerialNumber} </div>  <div style='display: inline - block; margin - left:85px; '> {resElevator[H].Status}</div> </div> <div style='display: inline - block; margin - left:85px; '> {resElevator[H].ElevatorType }</div>";
@@ -127,14 +129,61 @@ namespace Customers_Portal.Controllers
         {
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var responseGet = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Buildings/customer/{id}");
-            List<BuildingModel> res = await responseGet.Content.ReadAsAsync<List<BuildingModel>>();
+            var responseGet = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Customers/id/{id}");
+            List<UesrModel> res = await responseGet.Content.ReadAsAsync<List<UesrModel>>();
+            ViewBag.customer = res[0];
+
+       
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+             responseGet = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Buildings/customer/{id}");
+            List<BuildingModel> resBuildings = await responseGet.Content.ReadAsAsync<List<BuildingModel>>();
 
             for (int i = 0; i < res.Count; i++)
             {
-                ViewBag.BuildingTable += $"<option value='{ res[i].id}'> {res[i].id} </option> ";
+                ViewBag.BuildingTable += $"<option value='{ resBuildings[i].id}'> {resBuildings[i].id} </option> ";
+            
+                ViewBag.BatteryTable = new List<BuildingModel>();
+
+                var responseGetBatteries = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Batteries/building/{res[i].id}");
+                List<BatteryModel> resBatteries = await responseGetBatteries.Content.ReadAsAsync<List<BatteryModel>>();
+
+
+                for (int j = 0; j < resBatteries.Count; j++)
+                {
+                    ViewBag.BatteriesTable += $"<option value='{ resBatteries[i].id}'> {resBatteries[i].id} </option> ";
+
+                    var responseGetColumns = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Columns/battery/{resBatteries[j].id}");
+                    List<ColumnModel> resColumns = await responseGetColumns.Content.ReadAsAsync<List<ColumnModel>>();
+
+                    for (int k = 0; k < resColumns.Count; k++)
+                    {
+                        ViewBag.ColumnsTable += $"<option value='{ resColumns[i].Id}'> {resColumns[i].Id} </option> ";
+
+                        var responseGetElevator = await httpClient.GetAsync(requestUri: $"https://sirinerocketelevatorsrestapi.azurewebsites.net/api/Elevators/columns/{resColumns[k].Id}");
+                        List<ElevatorModel> resElevator = await responseGetElevator.Content.ReadAsAsync<List<ElevatorModel>>();
+ 
+                        for (int H = 0; H < resElevator.Count; H++)
+                        {
+                            ViewBag.ElevatorTable += $"<option value='{ resElevator[H].Id}'> resElevator[H].Id </option> ";
+
+                        }
+                        ViewBag.ElevatorTable += $"</table> ";
+                    }
+                    ViewBag.ColumnsTable += $"</table> <br/> <br/>";
+
+
+                }
+                ViewBag.BatteriesTable += $"<table > <br/> <br/>";
+                // ViewBag.BatteriesTable += $" </table>"; 
+
+
             }
+            ViewBag.BuildingTable += $"<table > <br/> <br/>";
             return View();
+
+
+
+
 
         }
     }
